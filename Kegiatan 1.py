c4de6590180cc96bdb2fc85df08afed139b7da54
L200184092 <44857363+L200184092@users.noreply.github.com>
2018-11-26 15:29:37 +0700

    Modul Praktikum 8
    Modul Praktikum 8

--- Kegiatan 1.py
+++ Kegiatan 1.py
@@ -1,16 +1,94 @@
-print("No |", "Nama Bangun     |", "Rumus Luas")
-print("---|-----------------|----------------")
-x = {1:"Segitiga", 2:"Persegi", 3:"Persegi Panjang", 4:"Lingkaran", 5:"Jajaran Genjang"}
-y = {"Segitiga":"L = 0.5 * a * t", "Persegi":"L = s ** 2", "Persegi Panjang":"L = p * 1", "Lingkaran":"L = pi * r ** 2", "Jajaran Genjang":"L = a * t"}
-for i in range(6):
-    if i == 1:
-        print("1  |", x[1], "       |", y["Segitiga"])
-    elif i == 2:
-        print("2  |", x[2], "        |", y["Persegi"])
-    elif i == 3:
-        print("3  |", x[3], "|", y["Persegi Panjang"])
-    elif i == 4:
-        print("4  |", x[4], "      |", y["Lingkaran"])
-    elif i == 5:
-        print("5  |", x[5], "|", y["Jajaran Genjang"])
-    
+Data = {"NIM":"L200184092", "Nama":"Ganno Tribuana Kurniaji", "Tempat/Tanggal_Lahir":"Surakarta / 7 November 1999", "Jenis_Kelamin":"Laki - laki", "Golongan_Darah":"A", "Alamat":"Genengan, Mojosongo, Jebres, Surakarta", "Agama":"Islam", "Status_Perkawinan":"Belum Kawin", "Pekerjaan":"Mahasiswa", "Kewarganegaraan":"Indonesia"}
+NIM = Data["NIM"]
+Nama = Data["Nama"]
+TTL = Data["Tempat/Tanggal_Lahir"]
+JK = Data["Jenis_Kelamin"]
+GD = Data["Golongan_Darah"]
+Alamat = Data["Alamat"]
+Agama = Data["Agama"]
+SP = Data["Status_Perkawinan"]
+Pekerjaan = Data["Pekerjaan"]
+Kewarganegaraan = Data["Kewarganegaraan"]
+
+print("Pilihan Yang Tersedia:")
+print("f menampilkan bantuan ini")
+print("z menampilkan NIM")
+print("x menampilkan Nama")
+print("c menampilkan Tempat/Tanggal Lahir")
+print("v menampilkan Jenis Kelamin")
+print("b menampilkan Golongan Darah")
+print("n menampilkan Alamat")
+print("m menampilkan Agama")
+print("a menampilkan Status Perkawinan")
+print("s menampilkan Pekerjaan")
+print("d menampilkan Kewarganegaraan")
+print("g untuk keluar")
+print(" ")
+
+f ='''Pilihan Yang Tersedia:
+f menampilkan bantuan ini
+z menampilkan NIM
+x menampilkan Nama
+c menampilkan Tempat/Tanggal Lahir
+v menampilkan Jenis Kelamin
+b menampilkan Golongan Darah
+n menampilkan Alamat
+m menampilkan Agama
+a menampilkan Status Perkawinan
+s menampilkan Pekerjaan
+d menampilkan Kewarganegaraan
+g untuk keluar'''
+
+g = "Terima Kasih"
+w = input("Masukkan huruf: ")
+while w != "g":
+    if w == "f":
+        print(f)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "z":
+        print(NIM)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "x":
+        print(Nama)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "c":
+        print(TTL)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "v":
+        print(JK)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "b":
+        print(GD)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "n":
+        print(Alamat)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "m":
+        print(Agama)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "a":
+        print(SP)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "s":
+        print(Pekerjaan)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "d":
+        print(Kewarganegaraan)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    else:
+        print("perintah tidak dikenal")
+        print(" ")
+        w = input("Masukkan huruf: ")
+print(g)
+
--- Kegiatan 2.py
+++ Kegiatan 2.py
@@ -1,13 +1,12 @@
-x = input("Masukkan Password : ")
-if x == "Ganno":
-    print("Anda Berhasil Login")
-else:
-    x = input("Maaf, Anda Salah Memeasukkan Password. Masukkan Password : ")
-    if x == "Ganno":
-        print("Anda Berhasil Login")
-    else:
-        x = input("Maaf, Anda Salah Memeasukkan Password. Masukkan Password : ")
-        if x == "Ganno":
-            print("Anda Berhasil Login")
-        else:
-            print("Anda Telah Mencoba 3 Kali, Akses Anda Ditolak")
+def konversiSuhu(C = "none", F = "none"):
+    if C == "none" and F == "none":
+            print("Suhu 0 Celcius setara dengan 32 Fahrenheit")
+    elif F == "none":
+        C1 = C * 9 / 5 + 32
+        C1 = int(C1)
+        print("Suhu", C, "Celcius setara dengan", C1, "Fahrenheit")
+    elif C == "none":
+        F1 = (F - 32) * 5 / 9
+        F1 = int(F1)
+        print("Suhu", F, "Fahrenheit setara dengan", F1, "Celcius")
+        
